1dd7e74348ebd46dff75f38eaf318a44a5c4b93f
Tex <techcenter2001@gmail.com>
2023-12-30 16:42:09 +0300

    file setup

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  loginForm: FormGroup;
 
-  constructor() {}
+  constructor(public formBuilder: FormBuilder) {
+    this.loginForm = formBuilder.group({
+      username: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern('^[a-zA-Z0-9]+$'),
+          Validators.maxLength(10),
+        ]),
+      ],
+      password: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(8)]),
+      ],
+    });
+  }
 
+  onLoginFormSubmit(value: any) {
+    alert('User Name: ' + value.username + ' Password: ' + value.password);
+  }
 }
